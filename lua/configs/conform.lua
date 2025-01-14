e46f79449d0fba9d5604c3ecec809d2bc503f405
Abylay Kuvatov <abukuvatov@icloud.com>
2025-01-14 09:04:05 +0500

    Added conform config

---
+++ lua/configs/conform.lua
@@ -0,0 +1,19 @@
+require("conform").setup({
+	formatters_by_ft = {
+		lua = { "stylua" },
+		python = { "autopep8", "isort", "black" },
+		javascript = { "prettierd", "prettier", stop_after_first = true },
+	},
+	format_on_save = {
+		-- These options will be passed to conform.format()
+		timeout_ms = 500,
+		lsp_format = "fallback",
+	},
+})
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = "*",
+	callback = function(args)
+		require("conform").format({ bufnr = args.buf })
+	end,
+})
